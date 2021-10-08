4cac2d0091605e985baf19171dda95515257640a
Guillermo Ignacio Enriquez Gutierrez <nacho4d@gmail.com>
2021-10-09 01:51:04 +0900

    Update for 2.2.0

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -1,9 +1,9 @@
 class Xchtmlreport < Formula
     desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
     homepage "https://github.com/XCTestHTMLReport/XCTestHTMLReport"
-    url "https://github.com/XCTestHTMLReport/XCTestHTMLReport/archive/2.1.0.tar.gz"
-    sha256 "98bc4d97c18b7bfa46ca9fbe72df2351fa96acabe70f14ec5b4f9aeca7bc5b57"
-    head "https://github.com/XCTestHTMLReport/XCTestHTMLReport.git", :branch => "develop"
+    url "https://github.com/XCTestHTMLReport/XCTestHTMLReport/archive/2.2.0.tar.gz"
+    sha256 "bc422443945255ed03a0af6e3f72ed342dd0d1b74dd0d641e6dd773aa6195823"
+    head "https://github.com/XCTestHTMLReport/XCTestHTMLReport.git", :branch => "main"
   
     def install
       system "swift build --disable-sandbox -c release"
