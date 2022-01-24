017f8c0e8ce237d619d8e6a5559b53ca2281a3ad
tylervick <tylervick@users.noreply.github.com>
2022-01-24 07:07:46 +0000

    2.2.1 formula version bump

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -2,7 +2,7 @@ class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/XCTestHTMLReport/XCTestHTMLReport"
   url "https://github.com/XCTestHTMLReport/XCTestHTMLReport/archive/refs/tags/2.2.1.tar.gz"
-  sha256 "812bb0655517a705a06a931a0a9b59c674064018e8c946dee81817c7a1df327a"
+  sha256 ""
   license "MIT"
   head "https://github.com/XCTestHTMLReport/XCTestHTMLReport.git", :branch => "main"
 
